a681647f2bcc53b009aeade6eb1d7ae19f9b966b
radhouene-massoudi <massoudiradhouene@gmail.com>
2024-07-03 19:46:28 +0100

    seance form

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Pro;
+use App\Form\AddproduitType;
 use App\Repository\ProRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +27,71 @@ class ProduitController extends AbstractController
         $studnets=$repo->findAll();
         return $this->render('produit/listStudents.html.twig',[
             's'=>$studnets
+        ]);
+    }
+    
+    #[Route('/add', name: 'add')]
+    public function addProduit(ManagerRegistry $mr)
+    {
+        $p=new Pro();
+        $p->setName('test statique');
+        $p->setPrice(55);
+        $p->setQuantity(77);
+        $em=$mr->getManager();
+        $em->persist($p);
+        $em->flush();
+        return $this->redirectToRoute('st');
+    }
+    #[Route('/addproduit', name: 'addproduit')]
+    public function addProduct(ManagerRegistry $mr, Request $req)
+    {
+        $p=new Pro();
+        
+        $formprouit=$this->createForm(AddproduitType::class,$p);
+        $formprouit->handleRequest($req);
+       if($formprouit->isSubmitted()){
+        $em=$mr->getManager();
+        $em->persist($p);
+        $em->flush();
+        return $this->redirectToRoute('st');
+       }
+        
+        return $this->render('produit/addproduit.html.twig',[
+            'f'=>$formprouit->createView()
+        ]);
+    }
+
+    #[Route('/update/{id}', name: 'u')]
+    public function updateProduct(ManagerRegistry $mr, Request $req,$id,ProRepository $repo)
+    {
+       // $p=new Pro();
+        $produit=$repo->find($id);
+        $formprouit=$this->createForm(AddproduitType::class,$produit);
+        $formprouit->handleRequest($req);
+       if($formprouit->isSubmitted()){
+        $em=$mr->getManager();
+        $em->persist($produit);
+        $em->flush();
+        return $this->redirectToRoute('st');
+       }
+        
+        return $this->render('produit/addproduit.html.twig',[
+            'f'=>$formprouit->createView()
+        ]);
+    }
+    #[Route('/updatetwo/{id}', name: 'two')]
+    public function updateProducttwo(ManagerRegistry $mr, Request $req, Pro $produit)
+    {
+               $formprouit=$this->createForm(AddproduitType::class,$produit);
+        $formprouit->handleRequest($req);
+       if($formprouit->isSubmitted()){
+        $em=$mr->getManager();
+               $em->flush();
+        return $this->redirectToRoute('st');
+       }
+        
+        return $this->render('produit/addproduit.html.twig',[
+            'f'=>$formprouit->createView()
         ]);
     }
 }
---
+++ src/Form/AddproduitType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pro;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddproduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('quantity')
+            ->add('price')
+            ->add('save',SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pro::class,
+        ]);
+    }
+}
